8641e23c0f6e8ec6cdd0c362b42642a56984557a
nickg <148371638+nickg-hmrc@users.noreply.github.com>
2026-01-12 10:36:27 +0000

    NO-TICKET: Remove redundant Python code (#15)
    Python was used to generate the original Markdown files for an earlier
    version of EMCS. Many modifications have been made by hand since then,
    and in version 4.2 the source documents (which the Python code scrapes)
    changed. The Python code is now redundant and just confuses people when
    they search the code, so it should be removed

--- python/html_utils.py
+++
@@ -1,9 +0,0 @@
-def createTableHeader(html, tr, title):
-    td = html.new_tag('th')
-    td.string = title
-    tr.append(td)
-
-def createTableData(html, tr, data):
-    td = html.new_tag('td')
-    td.string = data
-    tr.append(td)
